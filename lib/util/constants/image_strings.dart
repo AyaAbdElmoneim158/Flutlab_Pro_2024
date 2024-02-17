48b5ac2280b338de41092e79e3135bb730dfe52d
AyaAbdElmoneim158 <ayaabdelmon@gmail.com>
2024-02-17 22:32:56 +0200

    ✔️ Add Constants

---
+++ lib/util/constants/image_strings.dart
@@ -0,0 +1,29 @@
+const String logosPath = 'assets/logos';
+const String imagesPath = 'assets/images';
+
+class TImage {
+  //Logos - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  static const String darkLogo = '$logosPath/dark_logo.png';
+  static const String lightLogo = '$logosPath/light_logo.png';
+  static const String logo = '$logosPath/goods.png';
+
+  //Images - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  static const String facebook = '$imagesPath/facebook.svg';
+  static const String google = '$imagesPath/google.svg';
+
+  static const String onboarding1 =
+      '$imagesPath/onboarding_images/onboarding1.png';
+  static const String onboarding2 =
+      '$imagesPath/onboarding_images/onboarding2.png';
+  static const String onboarding3 =
+      '$imagesPath/onboarding_images/onboarding3.png';
+
+  static const String emailVerify = '$imagesPath/email_verify.png';
+  static const String search = '$imagesPath/search.png';
+  static const String image1 = '$imagesPath/image1.png';
+  static const String image2 = '$imagesPath/image2.png';
+  static const String image3 = '$imagesPath/image3.png';
+  static const String image4 = '$imagesPath/image4.png';
+  static const String image5 = '$imagesPath/image5.png';
+  static const String image6 = '$imagesPath/image6.png';
+}
---
+++ lib/util/constants/text_strings.dart
@@ -0,0 +1,62 @@
+class TTexts {
+  static const String submit = "Submit";
+  static const String appName = "T-Store";
+  static const String tcontinue = "Continue";
+
+  //Onboarding Texts - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  static const String onboardingTitle1 = 'Choose your Product';
+  static const String onboardingTitle2 = 'Select Payment Method';
+  static const String onboardingTitle3 = 'Deliver at your door step';
+  static const String onboardingSubTitle1 =
+      'Welcome to a world of limitations Choices - your Perfect Product Awaits!';
+  static const String onboardingSubTitle2 =
+      'For Seamless Transactions, Choose Your Payment Path - your Convenience, Our Priority!';
+  static const String onboardingSubTitle3 =
+      'From Our Doorstep to your - Swift, Secure, and Contactless Delivery!';
+
+  //Home Texts - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  static const String homeAppbarTitle = 'Good day for shopping';
+  static const String homeAppbarSubTitle = 'Aya Abdelmoneim';
+
+  //Authentication - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  static const String firstName = "First Name";
+  static const String lastName = "Last Name";
+  static const String email = "E-Mail";
+  static const String password = "Password";
+  static const String newPassword = "New Password";
+  static const String username = "Username";
+  static const String phoneNo = "Phone Number";
+  static const String rememberMe = "Remember Me";
+  static const String forgetPassword = "Forget Password?";
+  static const String signIn = "Sign In";
+  static const String createAccount = "Create Account";
+  static const String orSignInWith = "or sign in with";
+  static const String orSignUpWith = "or sign up with";
+  static const String iAgreeTo = "I agree to";
+  static const String privacyPolicy = 'Privacy Policy';
+  static const String termsOfUse = "Terms of use";
+  static const String verificationCode = "verificationCode";
+  static const String resendEmail = "Resend Email";
+  static const String resendEmailIn = "Resend email in";
+  static const String and = "and";
+
+
+  static const String loginTitle = "Welcome Back";
+  static const String loginSupTitle =
+      "Discover Limitless Choices and unmatched Convenience.";
+  static const String signUpTitle = "Let's create your account";
+  static const String forgetPasswordTitle = "Forget password";
+  static const String forgetPasswordSubTitle =
+      "Don't worry sometimes people can forget too, enter your email and we will send you a password reset Link.";
+  static const String changeYourPasswordTitle = "Password Reset Email Sent";
+
+static const String changeYourPasswordSubTitle = "Your Account Security is Our Priority! we've sent. You a Secure Link to Safety Change Your Password and Keep Your Account Protected."; 
+static const String confirmEmail = "verify your email address!";
+static const String confirmEmailSupTitle = "Congratulations! Your Account Assists: Verify Your Email to start Shopping and Experience a world of services Deals and Personalized Offers.";
+ static const String emailNotReceivedMessage = "Didn't get the email? Check your junk/span or resend it."; 
+ static const String yourAccountCreatedTitle = "Your account successfully created";
+ static const String yourAccountCreatedSubtitle ="Welcome to Your Ultimate Shopping Destination: Your Account is Created, Unleash the Joy of Seamless Online Shopping!";
+ 
+}
+
